f06b0897b88f64caed5c5aec89cbdbd64e8514e7
Aina Rafaly <ramavoharinaivofalynyaina@gmail.com>
2021-04-02 21:13:29 +0300

    update

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + '/src'));
 
 app.get('/*', function(req, res){
-  res.sendFile(path.join(__dirname + '/index.html'));
+  res.sendFile(path.join(__dirname + '/src/index.html'));
 })
 
 app.listen(process.env.PORT);
